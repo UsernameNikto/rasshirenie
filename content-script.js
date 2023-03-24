175da32a3a510f23f4ad70be744bfe6fb9344557
Anisimov Vyacheslav <92636823+UsernameNikto@users.noreply.github.com>
2023-03-24 18:34:57 +0500

    Add files via upload

---
+++ content-script.js
@@ -0,0 +1,13 @@
+function VaBlock(){
+    !!document
+    .getElementsByClassName(
+        "ContributionCalendar-day"
+    )
+    &&
+    document
+    .getElementsByClassName(
+        "ContributionCalendar-day"
+    )
+    [0].style.backgroundColor=="#4B0082";
+}
+VaBlock();
